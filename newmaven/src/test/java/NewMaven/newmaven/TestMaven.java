b2a486e9f86e653f750d352f37ace06170808240
mergertesting2 <mergertesting2@mergertestings>
2017-05-04 10:50:43 +0530

    fLIPKart Testing using maven

--- newmaven/src/test/java/NewMaven/newmaven/TestMaven.java
+++ newmaven/src/test/java/NewMaven/newmaven/TestMaven.java
@@ -1,11 +1,27 @@
 package NewMaven.newmaven;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestMaven {
 
 	@Test
 	  public void verify() {
-		System.out.println("Trying differently in new maven");
+		
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		//System.setProperty(
+		         // FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,
+		        //  "true");
+		
+		//System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
+		//WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.flipkart.com/");
+		driver.close();
 	}
 }
